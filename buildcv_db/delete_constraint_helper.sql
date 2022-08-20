eb6068413cadde72fcbc50d589bf655b9cc17bbe
Abdullah Mohammed <a77x86@gmail.com>
2022-08-20 20:07:57 +0400

    Add Helper Script for deleting constranits

---
+++ buildcv_db/delete_constraint_helper.sql
@@ -0,0 +1,14 @@
+-- Helper Script for deleting constraints 
+
+-- USE resume;  
+-- GO  
+-- -- Return the name of primary key.  
+-- SELECT name  
+-- FROM sys.key_constraints  
+-- WHERE type = 'PK' AND OBJECT_NAME(parent_object_id) = N'users';  
+-- GO  
+-- alter table users 
+-- drop constraint PK__users__3214EC0725AB0578 ;
+-- go 
+-- alter table address 
+-- drop constraint FK__address__user_id__4CA06362
